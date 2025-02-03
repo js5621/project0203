f639119fbe2d24db8a6f78c5a976b8ba44a9c828
js5621 <asaph5621@gmail.com>
2025-02-03 12:34:35 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!2");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write('*');
+                }
+                Console.Write('\n');
+            }
         }
     }
 }
